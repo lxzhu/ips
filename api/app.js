818692bd2617dd8b39a7d0e6ef535e8dfd497106
Zhu Liangxiong <lxzhu@outlook.com>
2014-11-27 18:41:25 +0800

    setup api/app.js

--- api/app.js
+++ api/app.js
@@ -1,2 +1,39 @@
 var conf=require('./conf.js');
-console.log("mysql.host:"+ conf.mysql.host);
+var express = require('express');
+
+var bodyParser=require('body-parser');
+var cookieParser=require('cookie-parser');
+var redirect = require('express-redirect');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+redirect(app);
+
+app.set('port', process.env.PORT || 12000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+//favicon is removed. could use https://www.npmjs.org/package/static-favicon
+//app.use(express.favicon()); 
+//TODO: how to config so it log to file?
+//app.use(express.logger('dev'));
+//app.use(express.json());
+//TODO: how to config it to allow url rewrite?
+//app.use(express.urlencoded());
+//app.use(express.query());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse application/json
+app.use(bodyParser.json())
+//app.use(express.methodOverride());
+//app.use(app.router);
+app.use('/docs', express.static(path.join(__dirname, 'docs')));
+app.redirect('/docs','/docs/index.html');
+app.redirect('/docs/','/docs/index.html');
+app.redirect('/','/docs/index.html');
+
+http.createServer(app).listen(app.get('port'), function(error) {
+	if (error) {
+		console.log(error);
+	}
+});

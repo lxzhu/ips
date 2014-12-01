2aef0770fd8c13592d7c993f2a55e9517fe1ca5c
anniexing <mxxing@live.com>
2014-12-01 19:29:52 +0800

    update

--- html/js/app.js
+++ html/js/app.js
@@ -1,3 +1,4 @@
-define(['angular','angularResource','app/controllers/mainController','app/services/mainService'],function(angular){
-	return angular.module('webApp',['angularResource','controllers','services','ui.router','ngCookies','ngRoute','ngAnimate'])
-})
+//for require js
+/*define(['angular','angularResource','angularUiRouter','uiBootstarpTpls','angularCookie','angularAnimate'],function(angular){
+	return angular.module('webApp',['ngResource','ui.router','ui.bootstrap','ngCookie','ngAnimate']);
+})*/
